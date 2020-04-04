391c0962420f0fbdf123be0dbb5c0791e2d0ccd4
Kevin Matos <mat18015@byui.edu>
2020-04-03 23:12:48 -0600

    env

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,6 @@
-const { pool } = require('../config')
+//const { pool } = require('../config')
+const { Pool, Client } = require('pg')
+const pool = new Pool()
 
 module.exports = function(app) {
 
@@ -7,6 +9,12 @@ module.exports = function(app) {
         var full = new Date();
         var dt = full.getFullYear();
         
+        pool.query('SELECT * FROM schedule', (err, res) => {
+            console.log(err, res)
+            pool.end()
+          })
+        
+        /*
         pool.query('SELECT * FROM schedule', (error, results) => {
             if (error) {
                 console.log("sup son")
@@ -16,7 +24,7 @@ module.exports = function(app) {
             console.log("you passed")
             Response.status(200).json(results.rows)
         })
-        
+        */
         
         res.render("home", {dt : dt});
     });
