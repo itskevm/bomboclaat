f939a28797afb036eed0a6b2d3fc24b329eafbb9
Kevin Matos <mat18015@byui.edu>
2020-04-03 23:26:12 -0600

    aun mejor

--- controllers/home.js
+++ controllers/home.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
         
         pool.query('SELECT * FROM schedule', (err, res) => {
             console.log("start me funky: ")
-            console.log(err, res)
+            console.log(res)
           })
         
         /*
