b06c481fa55e3f62b63e2e860a9b1d8e0118cb11
Kevin Matos <mat18015@byui.edu>
2020-04-03 23:05:55 -0600

    a ver

--- controllers/home.js
+++ controllers/home.js
@@ -6,17 +6,17 @@ module.exports = function(app) {
         console.log("Root was requested");
         var full = new Date();
         var dt = full.getFullYear();
-        /*
+        
         pool.query('SELECT * FROM schedule', (error, results) => {
             if (error) {
-                console.log("sup")
+                console.log("sup son")
                 throw error
                 
             }
-
+            console.log("you passed")
             Response.status(200).json(results.rows)
         })
-        */
+        
         
         res.render("home", {dt : dt});
     });
