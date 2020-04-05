ca9005914e10075ad8318780630e3b1707c7bbd7
Kevin Matos <mat18015@byui.edu>
2020-04-05 14:19:55 -0600

    ojala

--- controllers/home.js
+++ controllers/home.js
@@ -1,20 +1,13 @@
 //const { pool } = require('../config')
-const { Pool, Client } = require('pg')
+const { Pool } = require('pg')
 const connectionString = 'postgres://wijeezpfrowzgj:0d742ba8ed25fae44c5999999385c5771bf52bd868791616a9c5b75e27232424@ec2-54-92-174-171.compute-1.amazonaws.com:5432/deal6623ob8hm2'
 const pool = new Pool({
     connectionString: connectionString,
+    ssl: true
 })
 
-function loadDb(err, result) {
-    console.log("start me funky: ")
-    console.log(err);
-    console.log(result.rows[0]);
-    console.log(result.rows.length);
-    //id = result.rows[0].schedule_id;
-    //name = result.rows[0].schedule_name;
-    //console.log(id);
-    //console.log(name);
-    return result.rows[0];
+function loadDb(err) {
+
 };
 
 module.exports = function(app) {
@@ -23,11 +16,19 @@ module.exports = function(app) {
         console.log("Root was requested");
         var full = new Date();
         var dt = full.getFullYear();
-        
-        var todo = pool.query('SELECT * FROM schedule', loadDb);
-        
-        var id = todo.schedule_id;
-        var name = todo.schedule_name;
+        var id = "777"
+        var name = "shaq daddy"
+
+        pool.query('SELECT * FROM schedule', function(err, result) {
+            console.log("start me funky: ")
+            console.log(err);
+            console.log(result.rows[0]);
+            console.log(result.rows.length);
+            id = result.rows[0].schedule_id;
+            name = result.rows[0].schedule_name;
+            console.log(id);
+            console.log(name);
+          })
         
         /*
         pool.query('SELECT * FROM schedule', (error, results) => {
